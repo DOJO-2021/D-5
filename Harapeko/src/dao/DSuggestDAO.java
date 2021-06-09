bb5a27ee55d4357cda7eb8c99eba916b8321ec9a
dojo0201-hata <hata@sepuls2016.onmicrosoft.com>
2021-06-09 14:58:25 +0900

    h

--- Harapeko/src/dao/DSuggestDAO.java
+++ Harapeko/src/dao/DSuggestDAO.java
@@ -24,15 +24,14 @@ public class DSuggestDAO {
 			Class.forName("org.h2.Driver");
 
 			// �f�[�^�x�[�X�ɐڑ�����
-			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/data/D-5/data", "sa", "sa");
+			conn = DriverManager.getConnection("jdbc:h2:file:C:/pleiades/workspace/D-5/data", "sa", "sa");
 
 			// SQL������������@�d�v���@�H��AND�@OR�ɂ��K�p���邩
 			//if (aaaa.getKensaku() == "AND") {ptst.setstring(3,getKensaku)
 
 			//�S�e�[�u���擾�A����
-			String sql = "Select distinct md.dish_id,md.dish_name,md.img_path,md.dish_genre,md.difficulty,md.cal,md.url"
-					+ " from ( dish_details as dd INNER JOIN m_dish as md  using(dish_id));"
-					+ "inner join m_food as mf using(food_id);";
+			String sql = "select distinct md.dish_id, md.dish_name, md.img_path, md.dish_genre, md.difficulty, md.cal, md.url from dish_details as dd, m_dish md, m_food mf"
+					+ " where dd.dish_id = md.dish_id AND dd.food_id = mf.food_id;";
 			//where CAL <= ? and DISH_GENRE = ? and DIFFICULTY = ? and FOOD_NAME = ?;
 			PreparedStatement pStmt = conn.prepareStatement(sql);
 
@@ -56,13 +55,13 @@ public class DSuggestDAO {
 			// ���ʕ\���R���N�V�����ɃR�s�[����
 			while (rs.next()) {				//Next�͎��̃f�[�^������A���̃f�[�^���������
 				Dish card = new Dish(
-				rs.getString("ID"),
-				rs.getString("NAME"),
-				rs.getString("PATH"),
-				rs.getString("GENRE"),
-				rs.getInt("CAL"),
-				rs.getString("DIFF"),
-				rs.getString("URL")
+				rs.getString("dish_ID"),
+				rs.getString("dish_name"),
+				rs.getString("img_path"),
+				rs.getString("dish_genre"),
+				rs.getInt("cal"),
+				rs.getString("difficulty"),
+				rs.getString("url")
 				);
 				dishList.add(card);		//63�s�ڂ�SearchServe�ɑ�����
 			}
