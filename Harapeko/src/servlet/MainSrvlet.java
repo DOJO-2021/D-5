65430d690b126b3b7ea8cb182445540c23977f82
dojo0201-hata <hata@sepuls2016.onmicrosoft.com>
2021-06-09 10:49:24 +0900

    a

--- Harapeko/src/servlet/MainSrvlet.java
+++ Harapeko/src/servlet/MainSrvlet.java
@@ -1,6 +1,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,12 +10,20 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import dao.FSuggestDAO;
+import model.Food;
+
 @WebServlet("/MainSrvlet")
 public class MainSrvlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//おすすめ食材を選ぶ
+		FSuggestDAO FDao = new FSuggestDAO();
+		List<Food> foodList = FDao.select(new Food("",""));
+		Math.random(foodList.size());
+
 
 		// メインページにフォワードする
 		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/main.jsp");
