8a18fb0d07c8f23a52bbebc1e11ede86e3f82686
dojo2021-mi-suzuki <suzuki-mi@seplus2016.onmicrosoft.com>
2021-06-16 11:44:58 +0900

    suzuki

---
+++ Harapeko/WebContent/column.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<jsp:include page="/WEB-INF/jsp/header.jsp"/>
+ <!--  	<link rel="stylesheet" href="/Harapeko/css/NewFile.css">-->
+</head>
+<body>
+	<header class="header">
+		<div class="logo">
+			<h1><img alt="はらぺこ。" src="/Harapeko/images/logo1_2.jpg"></h1>
+		</div>
+	</header>
+<main>
+	<div class="wrapper">
+		<h2 class="heading">豆知識</h2>
+
+
+	</div>
+</main>
+<jsp:include page="/WEB-INF/jsp/footer.jsp"/>
---
+++ Harapeko/src/servlet/ColumnServlet.java
@@ -0,0 +1,46 @@
+package servlet;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.FSuggestDAO;
+import model.Food;
+
+/**
+ * Servlet implementation class ColumnServlet
+ */
+@WebServlet("/ColumnServlet")
+public class ColumnServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// コラムをランダムで1つ表示
+		FSuggestDAO FDao = new FSuggestDAO();
+		List<Food> foodList = FDao.select();
+		Food recommend = foodList.get((int)(Math.random() * foodList.size()));
+		request.setAttribute("recomend", recommend.getName() );
+
+		// メインページにフォワードする
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/jsp/column.jsp");
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
