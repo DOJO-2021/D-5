7abfdeb726e7b7cc06a4803db0fd2ad1f3228f72
dojo2021-Kurikawa <kurikawa@seplus2016.onmicrosoft.com>
2021-06-16 16:25:57 +0900

    columnDAOTest、Colume.java更新

--- Harapeko/src/test/ColumnDAOTest.java
+++ Harapeko/src/test/ColumnDAOTest.java
@@ -1,5 +1,25 @@
 package test;
+import java.util.List;
+
+import dao.ColumnDAO;
+import model.Column;
+
 
 public class ColumnDAOTest {
 
+		public static void main(String[] args) {
+			ColumnDAO dao = new ColumnDAO();
+
+			// select()のテスト
+			System.out.println("---------- select()のテスト ----------");
+			List<Column> columnList2 = dao.select();
+			for (Column column : columnList2) {
+				System.out.println(column.getId());
+				System.out.println(column.getTitle());
+                                System.out.println(column.getContent());
+
+				System.out.println();
+			}
+		}
 }
+
